bad0dd4078f92cb6c74461536721ced259ae71b9
Javier Ezpeleta <javierezpeleta@gmail.com>
2022-06-28 19:49:52 +0200

    adding link to Github repo (with icon)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
 
 import { useThree } from "../hooks/useThree";
 import ThreeApp from "../threejs/ThreeApp";
@@ -9,18 +9,31 @@ export default function Home() {
   return (
     <div className="relative">
       <div className="absolute top-0 border-2 opacity-80">
-        <div className="text-red-500 bg-white w-72 m-4 p-4 rounded-md shadow-lg">
-          F: flip Pepe
-          <br />
-          S: spin Pepe
-          <br />
-          ARROWS: move Pepe
-          <br />
-          SPACE: spin all objects
-          <br />
-          ENTER: move Pepe forward
-          <br />
-          SHIFT: move Pepe backward
+        <div className="relative">
+          <div className="absolute top-3 right-6">
+            <a
+              href="https://github.com/JaviEzpeleta/threejs-nextjs-template"
+              target="_blank"
+            >
+              <FaGithub
+                size="32"
+                className="text-red-300 hover:text-red-400 transition-all"
+              />
+            </a>
+          </div>
+          <div className="text-red-500 bg-white w-72 m-4 p-4 rounded-md shadow-lg">
+            F: flip Pepe
+            <br />
+            S: spin Pepe
+            <br />
+            ARROWS: move Pepe
+            <br />
+            SPACE: spin all objects
+            <br />
+            ENTER: move Pepe forward
+            <br />
+            SHIFT: move Pepe backward
+          </div>
         </div>
       </div>
       <div ref={canvas} />
